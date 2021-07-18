febb54d44d96f57b18489f266b035fb5bf220903
Ganesh <ganeshgavhad9@gmail.com>
2021-07-18 11:40:33 +0530

    Added : UC1 Simple Linked List

--- LinkedList.java
+++ LinkedList.java
@@ -2,13 +2,44 @@ package com.bridgelabz.linkedlist;
 
 /**
  * Purpose: Implementation of Linked List Data Structure
- * @author Ganesh Gavhad
- * Version 1.0
+ * 
+ * @author Ganesh Gavhad Version 1.0
  * @param <T>
  */
 public class LinkedList<T> {
 
 	MyNode<T> head;
 
-}
+	/**
+	 * Adding Node to the linkedList
+	 * 
+	 * @param data is generic type 
+	 */
+	public void add(T data) {
+		MyNode<T> newNode = new MyNode<>();
+		newNode.data = data;
+		if (head == null) {
+			head = newNode;
 
+		} else {
+			MyNode<T> node = head;
+			while (node.next != null) {
+				node = node.next;
+			}
+			node.next = newNode;
+		}
+	}
+
+	/**
+	 * @show is a method for Printing The Data Of Node till next equal to null
+	 */
+	public void show() {
+		MyNode<T> node = head;
+		while (node.next != null) {
+			System.out.println(node.data);
+			node = node.next;
+		}
+		System.out.println(node.data);
+	}
+
+}
--- Runner.java
+++ Runner.java
@@ -12,7 +12,11 @@ public class Runner<T> {
 	 * @param args This is main method.
 	 */
 	public static void main(String[] args) {
-		
+	LinkedList <Integer> obj = new LinkedList<Integer>();
+		obj.add(56);
+		obj.add(30);
+		obj.add(70);
+		obj.show();
 		
 	}
 }
