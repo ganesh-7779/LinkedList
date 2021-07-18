38b65d1a19c8aee530e808df2ad50968d96a7c65
Ganesh <ganeshgavhad9@gmail.com>
2021-07-18 12:11:05 +0530

    Added : UC3 which is add new node to the previous node by Appending

--- Runner.java
+++ Runner.java
@@ -13,9 +13,10 @@ public class Runner<T> {
 	 */
 	public static void main(String[] args) {
 		LinkedList<Integer> obj = new LinkedList<Integer>();
+		obj.add(56);
+		obj.add(30);
 		obj.add(70);
-		obj.addAtStart(30);
-		obj.addAtStart(56);
+		
 		obj.show();
 	}
 }
